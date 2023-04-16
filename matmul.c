110edf97a165d68737873a24e6b39c5764a403a7
Ebaad Imran <imran.ebaad@yahoo.com>
2023-04-16 17:40:42 -0500

    Finished matrix math except for derivatives

--- matmul.c
+++ matmul.c
@@ -1,22 +1,93 @@
-float** matmul(float ** a, float** b, int n, int m, int k) {
-  // a is n*m and b is m*k, output is n*k 
-  float ** output = (float**)malloc(sizeof(float*) * n);
-  for(int i=0;i<n;i++) output[i] = (float*)(calloc(sizeof(float) * k));
-  for(int r=0;r<n;r++)
-    for(int c=0;c<k;c++)
-      for(int j=0;j<m;j++)
-	output[r][c] += a[r][j] * b[j][c];
-  return output;
-}
+float** matrix_mul(float ** a, float* b, int n, int m) {
+  float* output = (float*) calloc(n, sizeof(float));
 
-
-float** sigmoid(float** mat, int n, int m) {
-  float ** output = (float**)malloc(sizeof(float*) * n);
-  for(int i=0;i<n;i++) output[i] = (float*)(calloc(sizeof(float) * k));
+  for(int r = 0; r < n; r++)
+    for(int c = 0; c < m; c++)
+	    output[r] += a[r][c] * b[c];
   
-  for(int i=0;i<n;i++)
-    for(int j=0;j<m;j++)
-      output[i][j] = 1.0/(1+exp(-mat[i][j]));
+  return output;
+}
+
+
+float* sigmoid(float* input, int n) {
+  float* output = (float*) malloc(sizeof(float) * n);
+  
+  for(int i = 0; i < n; i++)
+    output[i] = 1.0/(1+exp(-input[i]));
 
   return output;
 }
+
+float* relu(float* input, int n) {
+  float* output = (float*) malloc(sizeof(float) * n);
+
+  for(int i = 0; i < n; i++)
+    output[i] = input[i] > 0 : input[i] ? 0;
+        
+  return output;
+}
+
+float* tanh(float* input, int n) {
+  float* output = (float*) malloc(sizeof(float) * n);
+
+  for(int i = 0; i < n; i++)
+    output[i] = (exp(input[i]) - exp(-input[i])) / (exp(input[i]) + exp(-input[i]));
+
+  return output;
+}
+
+float* softmax(float* input, int n) {
+  float* output = (float*) malloc(sizeof(float) * n);
+
+  float sum = 0;
+  for(int i = 0; i < n; i++)
+    sum += exp(input[i]);
+  for(int i = 0; i < n; i++)
+    output[i] = exp(input[i])/sum;
+  
+  return output;
+}
+
+float* vector_add(float* a, float* b, int n) {
+  float* output = (float*) malloc(sizeof(float) * n);
+
+  for(int i = 0; i < n; i++)
+    output[i] = a[i] + b[i];
+
+  return output;
+}
+
+float** hadamard_mul(float** a, float** b, int n, int m) {
+  float** output = (float**) malloc(sizeof(float*) * n);
+  for(int i = 0; i < n; i++) output[i] = malloc(sizeof(float) * m);
+
+  for(int r = 0; r < n; r++)
+    for(int c = 0; c < m; c++)
+      output[r][c] = a[r][c] * b[r][c];
+
+  return output;
+}
+
+float** matrix_trans(float** input, int n, int m) {
+  float** output = (float**) malloc(sizeof(float*) * m);
+  for(int i = 0; i < n; i++) output[i] = malloc(sizeof(float) * n);
+
+  for(int r = 0; r < m; r++)
+    for(int c = 0; c < n; c++)
+      output[r][c] = input[c][r];
+
+  return output;
+}
+
+float** matrix_scalar(float** input, int c, int n, int m) {
+  float** output = (float**) malloc(sizeof(float*) * n);
+  for(int i = 0; i < n; i++) output[i] = malloc(sizeof(float) * m);
+
+  for(int r = 0; r < n; r++)
+    for(int c = 0; c < m; c++)
+      output[r][c] = c * input[r][c];
+
+  return output;
+}
+
+// Derivative of Activations
